860938eb8b0ab4678ad498815ae54a72fc8238b1
aryal05 <aryal.rajat05@gmail.com>
2024-09-02 20:38:15 +0545

    background ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import FloatingShap from './components/FloatingShap'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>Hello world</h1>
-    </div>
+  <>
+  <div className='min-h-screen bg-gradient-to-br
+   from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
+    <FloatingShap
+    color = "bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0}
+    />
+    <FloatingShap
+    color = "bg-green-500" size="w-48 h-48" top="70%" left="80%" delay={5}
+    />
+
+<FloatingShap
+    color = "bg-green-500" size="w-32 h-32" top="50%" left="-10%" delay={2}
+    />
+
+
+  </div>
+  
+  </>
   )
 }
 
